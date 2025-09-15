6c4c9fffe921c685945d54996d98ca7ab9e2d9dc
Cedric <cedric.oleon@apside.com>
2025-09-15 11:20:24 +0200

    fonctionnement flyway V__ et R__ OK

--- backend/src/main/resources/db/migration/V2__seed_races.sql
+++ backend/src/main/resources/db/migration/V2__seed_races.sql
@@ -2,6 +2,5 @@ INSERT INTO races (code, libelle, description, caracs) VALUES
  ('HUMAIN','Humain','Polyvalent, adaptable',      '{"force":5,"dexterite":5,"intelligence":5,"vie":10}'),
  ('ELFE_BLEU','Elfe Bleu','Agile et affûté',      '{"force":3,"dexterite":7,"intelligence":6,"vie":9}'),
  ('ELFE_NOIR','Elfe noir','Sournoi et discret',   '{"force":3,"dexterite":7,"intelligence":8,"vie":7}'),
- ('NAIN','Nain','Solide et tenace',               '{"force":7,"dexterite":4,"intelligence":5,"vie":12}'),
- ('ORC','Orc','Puissant mais fruste',             '{"force":8,"dexterite":3,"intelligence":3,"vie":11}')
+ ('NAIN','Nain','Solide et tenace',               '{"force":7,"dexterite":4,"intelligence":5,"vie":12}')
 ON CONFLICT (code) DO NOTHING;
